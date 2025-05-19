679b192db4b253e78b66c62274253ef817685b8c
manuelgarfia0 <manuel.garfia@iesnervion.es>
2025-05-19 12:46:46 +0200

    Creacion Repo

---
+++ Tema7CursosDAO/src/entidades/Curso.java
@@ -0,0 +1,17 @@
+package entidades;
+
+public class Curso {
+
+	private int id;
+	private String nombre;
+	private String descripcion;
+	private String añoEscolar;
+	
+	public Curso(String nombre, String descripcion, String añoEscolar) {
+		super();
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.añoEscolar = añoEscolar;
+	}
+	
+}
